68556a1d3ba6879af274f72968e092280963746c
dcosovan <david.cosovan@leviatan.ro>
2022-11-04 15:14:08 +0200

    .\tests\

--- config/db.php
+++ config/db.php
@@ -2,10 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:server= localhost;dbname=user',
     'username' => 'root',
     'password' => '',
-    'charset' => 'utf8',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
