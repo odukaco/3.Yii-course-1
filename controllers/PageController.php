a2e89fe0c5e3cda767bac4dae75d6435d701d4c5
dcosovan <david.cosovan@leviatan.ro>
2022-11-04 14:02:28 +0200

    .\tests\

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -1,3 +1,11 @@
 <?php
 namespace app\controllers;
 use yii\web\Controller;
+
+// class PageController extends Controller
+// {
+//     public function actionAbout()
+//     {
+//         return $this->renderContent('<h1>Hello</h1>');
+//     }
+// }
